aeba8bc2d16d879fce4874dccb11f6d2f8a031b4
Moye <2324964154@.qqcom>
2024-09-28 19:20:21 +0800

    Commit MoyeClassActor additions

---
+++ Source/MyCPPProjTest/Private/MoyeClassActor.cpp
@@ -0,0 +1,29 @@
+// Copyright Moye practice. All Rights Reserved.
+
+
+#include "MoyeClassActor.h"
+
+
+// Sets default values
+AMoyeClassActor::AMoyeClassActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMoyeClassActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMoyeClassActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
